4d74cffac1e9a29b2e6ad951c5ea001e05a6c161
Norman Schmidt <n.schmidt.ge@gmail.com>
2021-12-04 15:49:48 +0100

    +Loadkeys and fix functional prog

--- install-archlinux.sh
+++ install-archlinux.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
 #################################
+######### CONFIGURATION #########
+##### (Later in Config File) ####
+#################################
+LOADKEY=de-latin1
+
+
+#################################
 ##### Function Definitions ######
 #################################
 
@@ -12,6 +19,12 @@ func_prologue() {
 	echo "Let's start"
 	sleep 1
 
+}
+
+func_loadkeys() {
+	echo -n "LoadKeys..."
+	loadkeys $LOADKEY
+	echo " done"
 }
 
 func_connect_to_wifi() {
@@ -43,48 +56,60 @@ func_internet_connection() {
 	fi
 }
 
+func_check_internet_connection() {
+	if ping -q -c 1 -W 1 8.8.8.8 >/dev/null; then
+	  	echo "IPv4 is up"
+	else
+	  	echo "IPv4 is down"
+	  	echo "Exiting..."
+	  	exit 1
+	fi
+
+	if ping -q -c 1 -W 1 google.com >/dev/null; then
+	  	echo "The network is up"
+	else
+	  	echo "The network is down"
+	  	echo "Exiting..."
+	  	exit 1
+	fi
+
+	case "$(curl -s --max-time 2 -I http://google.com | sed 's/^[^ ]*  *\([0-9]\).*/\1/; 1q')" in
+		[23])	echo "HTTP connectivity is up";;
+		5)    	echo "The web proxy won't let us through";;
+		*)    	echo "The network is down or very slow"
+		   	echo "Exiting..."
+		   	exit 1
+		   	;;
+	esac
+
+	echo "You have an Internet Connection :D"
+	sleep 1
+}
+
+func_release_notes() {
+	echo "Welcome to ArchLinux-Installer by NormannatoR"
+	sleep 1
+}
+
 #########################
 ##### Main Methode ######
 #########################
 echo "Welcome to ArchLinux-Installer by NormannatoR"
 sleep 1
 
-### Prologue
-func_prologue
+### Prologue ###
+#func_prologue
 
-### INTERNET CONNECTION
-func_internet_connection
+### LoadKeys ###
+#func_loadkeys
 
-### Check internet Connection
-if ping -q -c 1 -W 1 8.8.8.8 >/dev/null; then
-  echo "IPv4 is up"
-else
-  echo "IPv4 is down"
-  echo "Exiting..."
-  exit 1
-fi
+### INTERNET CONNECTION ###
+#func_internet_connection
 
-if ping -q -c 1 -W 1 google.com >/dev/null; then
-  echo "The network is up"
-else
-  echo "The network is down"
-  echo "Exiting..."
-  exit 1
-fi
-
-case "$(curl -s --max-time 2 -I http://google.com | sed 's/^[^ ]*  *\([0-9]\).*/\1/; 1q')" in
-  [23]) echo "HTTP connectivity is up";;
-  5) echo "The web proxy won't let us through";;
-  *) echo "The network is down or very slow"
-  echo "Exiting..."
-  exit 1
-  ;;
-esac
-
-echo "You have an Internet Connection :D"
-sleep 1
+### Check internet Connection ###
+#func_check_internet_connection
 
 ### Hinweise - Note ###
-echo "Note: This is a pre-alpha demo, feel free to contact me, if you run in trouble :)"
-sleep 5
+#func_release_notes
+
 
