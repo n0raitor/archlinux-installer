3e098922f476941699a4b10a1984fb5e167dc85d
normannator <n.schmidt.ge@gmail.com>
2022-02-22 23:39:28 +0100

    Fix UTF

--- install-archlinux.sh
+++ install-archlinux.sh
@@ -13,7 +13,7 @@ HOSTNAME=myhost
 KEYMAP=de-latin1
 FONT=lat9w-16
 LANG=de_DE.UTF-8
-LOCALE=("de_DE.UTF-8" "de_DE ISO-8859-1" "de_DE@euro ISO-8859-15" "en_US.UTF-8")
+LOCALE=("de_DE.UTF-8 UTF-8" "de_DE ISO-8859-1" "de_DE@euro ISO-8859-15" "en_US.UTF-8")
 TIMEZONE=/usr/share/zoneinfo/Europe/Berlin
 
 
