433fb0f87725df0166f9c5764c33c3b6635f8501
n0raitor <n0raitor@pm.me>
2022-04-19 18:51:18 +0200

    Add Configs and Instruction Script till Ulauincher for Macoz

--- desktop-environments/kde-plasma/install-macoz-Montereyz-theme-bundle_v1.sh
+++ desktop-environments/kde-plasma/install-macoz-Montereyz-theme-bundle_v1.sh
@@ -54,3 +54,48 @@ echo "Rename Theme (in ~/.themes) to Colloid-light-00, reopen the get new.. and 
 
 read -p "Configure like described and press ENTER to continue"
 
+mkdir -p ~/.fonts
+cp Resources/Montereyz/Fonts/* ~/.fonts/
+
+echo "Global Tehem: Fonts: Adjust all Fonts:"
+echo "- Font: SF Pro Display"
+echo "- Font Style: Regular"
+echo "On Fixed width: Hack 10pt"
+
+read -p "Configure like described and press ENTER to continue"
+
+echo "Config Themes and Fonts Done"
+sleep 3
+
+echo "Right Mouse Key on Desktop and Choose Walpaper -> Get new Wallpaper PLUGIN:"
+echo "- Inactive Blur (V4)"
+echo "Choose this Plugin in the Settings"
+echo "Choose the Monterey Wallpaper in the selection images"
+
+read -p "Configure like described and press ENTER to continue"
+
+echo "Settings -> Appearance -> Global Theme: Select Monterey"
+echo "-> Application Style: kvantum"
+echo "--> (Select GTK Style:) GTK Theme: Colloid-light"
+
+read -p "Configure like described and press ENTER to continue"
+
+echo "Kvantum and load Theme (./Resources/Montereyz/Monterey)"
+echo "Click: Install this Theme"
+echo "Switch to \"Change/Delete Theme\""
+echo "- Select Monterey as the Theme and click \"Use this theme\""
+
+read -p "Configure like described and press ENTER to continue"
+
+echo "Open \"Window Decorations\" (Global Theme):"
+echo "Remove everything from the top bar except the 3 Dots and place them like mac (X V A)"
+echo "Open \"Lock Screen\" (Workspace Behavior):"
+echo "Configure Appearance: Monterey"
+echo "Login Screen (SFFM): Monterey SDDM Theme (Light)(Get New SDDM Themes...) Debug: If it shows, that the theme is already installed, click uninstall but do not enter the password, quit the promt and now you should be able to install the light theme"
+
+read -p "Configure like described and press ENTER to continue"
+
+echo "Themes and Icons are configured"
+sleep 3
+
+# ULauncher
